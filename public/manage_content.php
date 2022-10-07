465ed1d58b25a71ba953f450853edbafd7a4709d
njcharlton <charltonnyaude@gmail.com>
2022-10-07 14:22:04 +0200

    initial

---
+++ public/manage_content.php
@@ -0,0 +1,48 @@
+
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php
+	// 2. Perform database query
+	$query  = "SELECT * FROM subjects WHERE visible = 1 ORDER BY position ASC";
+	$result = mysqli_query($connection, $query);
+   confirm_query($result);
+	// Test if there was a query error
+	if (!$result) {
+      die("Database query failed.");	
+	} 
+?>
+<?php include("../includes/layouts/header.php"); ?>
+
+<div id="main">
+   <div id="navigation">
+   <ul>
+		<?php
+			// 3. Use returned data (if any)
+			while($subject = mysqli_fetch_assoc($result)) {
+			// output data from each row
+		?>
+		
+		<li>
+			<?php 
+				echo $subject["menu_name"]." (".$subject["id"].")"; 
+            ?>
+		</li>
+	  <?php
+			}
+		?>
+		</ul>
+   </div>
+   <div id="page">
+     <h2>Manage Content</h2>
+
+   </div>
+</div>  
+<?php
+		  // 4. Release returned data
+		  mysqli_free_result($result);
+		?>
+
+<?php
+ include("../includes/layouts/footer.php");
+ ?>
+  
