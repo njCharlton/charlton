eff1fbd1d73517cf09fd6b7fcb8911862773d6de
njcharlton <charltonnyaude@gmail.com>
2022-10-11 20:42:53 +0200

    functional pulling subjects and pages

--- public/manage_content.php
+++ public/manage_content.php
@@ -2,6 +2,19 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
 
+<?php
+if (isset($_GET["subject"])) {
+	$selected_subject = $_GET["subject"];
+	$selected_page = null;
+} elseif (isset($_GET["page"])) {
+	$selected_subject = null;
+	$selected_page = $_GET["page"];
+} else {
+	$selected_subject = null;
+	$selected_page = null;
+}
+
+?>
 
 <div id="main">
 	<div id="navigation">
@@ -11,19 +24,21 @@
 			//var_dump($subject_set);
 			while ($subject = mysqli_fetch_assoc($subject_set)) {
 			?>
-				<li>
-					<?php echo $subject["menu_name"] . " " . $subject["id"];
+				<li class="selected">
+				 <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"] . " " . $subject["id"];
 
 					$page_set = find_all_pages_subject($subject["id"]);
 					//var_dump($page_set);
 
-					?>
+					?></a>
 					<ul class="pages">
 						<?php
 						while ($page = mysqli_fetch_assoc($page_set)) {
 						?>
 							<li>
-								<?php echo $page["menu_name"]; ?>
+								<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?> ">
+									<?php echo $page["menu_name"]; ?>
+								</a>
 							</li>
 						<?php
 						}
@@ -39,7 +54,7 @@
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
-
-
+		<?php echo $selected_subject; ?><br />
+		<?php echo $selected_page; ?>
 	</div>
 </div>
