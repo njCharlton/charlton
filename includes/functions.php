6282476df70c3c0be7e12e5824b5bdb87390c2cf
njcharlton <charltonnyaude@gmail.com>
2022-10-11 16:09:28 +0200

    functional pulling subject and pages

--- includes/functions.php
+++ includes/functions.php
@@ -8,11 +8,22 @@ function confirm_query($result_set)
 }
 function find_all_subjects()
 {
-    global $connection;
+    //get database connection
+    global $connection ;
+    //var_dump($connection);
 
+   //create sql query
     $query  = "SELECT * FROM subjects WHERE visible = 1 ORDER BY position ASC";
+
+    //send query to database and get result
     $subject_set = mysqli_query($connection, $query);
+
+    //check if result has no value
     confirm_query($subject_set);
+    // var_dump($subject_set);
+    // die();
+
+    //return value to be used
     return $subject_set;
 }
 
@@ -20,7 +31,10 @@ function find_all_pages_subject($subject_id)
 {
     global $connection;
 
-    $query1  = "SELECT * FROM pages WHERE visible = 1 and subject_id = " . $subject_id["id"] . " ORDER BY position ASC";
+    $query1  = "SELECT * FROM pages WHERE visible = 1 and subject_id = ".$subject_id." ORDER BY position ASC";
     $pages_set = mysqli_query($connection, $query1);
     confirm_query($pages_set);
+    // var_dump($pages_set);
+    // die();
+    return $pages_set;
 }
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,59 +1,45 @@
-
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-<?php
-	
-	$subject_set = find_all_subjects()
-	
-?>
 <?php include("../includes/layouts/header.php"); ?>
 
+
 <div id="main">
-   <div id="navigation">
-   <ul>
-		<?php
-			while($subject = mysqli_fetch_assoc($result)) {
-			
-		?>
-		
-		<li>
-			<?php 
-				echo $subject["menu_name"]." (".$subject["id"].")"; 
+	<div id="navigation">
+		<ul class="subjects">
+			<?php $subject_set = find_all_subjects(); ?>
+			<?php
+			//var_dump($subject_set);
+			while ($subject = mysqli_fetch_assoc($subject_set)) {
 			?>
-			<?php $page_set =
-			 find_all_pages_subject ($subject["id"]); ?>	
-			<ul class="pages">
-				<?php
-					while($page = mysqli_fetch_assoc($pages_set)) {
-				?>
 				<li>
-						<?php
-							echo $page["menu_name"];
-						?>
-				</li>
-				<?php
-					}
+					<?php echo $subject["menu_name"] . " " . $subject["id"];
+
+					$page_set = find_all_pages_subject($subject["id"]);
+					//var_dump($page_set);
+
 					?>
-			</ul>
-		</li>
-	  <?php
-
+					<ul class="pages">
+						<?php
+						while ($page = mysqli_fetch_assoc($page_set)) {
+						?>
+							<li>
+								<?php echo $page["menu_name"]; ?>
+							</li>
+						<?php
+						}
+						?>
+						<?php mysqli_free_result($page_set); ?>
+					</ul>
+				</li>
+			<?php
 			}
-		?>
+			?>
+			<?php mysqli_free_result($subject_set); ?>
 		</ul>
-   </div>
-   <div id="page">
-     <h2>Manage Content</h2>
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
 
-   </div>
-</div>  
-<?php
-		  // 4. Release returned data
-		  mysqli_free_result($result);
-		  mysqli_free_result($pages_set);
-		?>
 
-<?php
- include("../includes/layouts/footer.php");
- ?>
-  
+	</div>
+</div>
