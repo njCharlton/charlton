37daec0b5100d7f1a86ea13beb946611940bbc65
njcharlton <charltonnyaude@gmail.com>
2022-10-17 00:17:10 +0200

    deleting a subject

--- public/edit_subject.php
+++ public/edit_subject.php
@@ -101,6 +101,10 @@ if (isset($_POST['submit'])) {
 		</form>
 		<br />
 		<a href="manage_content.php">Cancel</a>
+		&nbsp;
+		&nbsp;
+		<a href="delete_subject.php?subject=<?php echo $current_subject["id"] ?>" onclick="return confirm('Are you sure?');">Delete subject</a>
+		
 	</div>
 </div>
 
