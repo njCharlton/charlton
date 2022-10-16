d54c423f3336bd797a1cd24c0bc5912a67af647e
njcharlton <charltonnyaude@gmail.com>
2022-10-16 15:33:29 +0200

    creating an edit subject form

--- public/manage_content.php
+++ public/manage_content.php
@@ -15,6 +15,7 @@
 		<?php if ($current_subject) { ?>
 	    <h2>Manage Subject</h2>
 			Menu name: <?php echo $current_subject["menu_name"]; ?><br />
+			<a href="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>">Edit Subject</a>
 			
 		<?php } elseif ($current_page) { ?>
 			<h2>Manage Page</h2>
@@ -27,4 +28,5 @@
 </div>
 
 <?php include("../includes/layouts/footer.php"); ?>
+
 
